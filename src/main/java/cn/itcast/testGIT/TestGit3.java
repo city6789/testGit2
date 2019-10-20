d3bea89a5080a369f329484e4ab34429aaaed301
heshiyuan163 <15801636814@163.com>
2019-10-20 15:45:41 +0800

    update TestGit3.java

--- src/main/java/cn/itcast/testGIT/TestGit3.java
+++ src/main/java/cn/itcast/testGIT/TestGit3.java
@@ -3,5 +3,6 @@ package cn.itcast.testGIT;
 public class TestGit3 {
     public static void main(String[] args){
         System.out.println("111111111111");
+        System.out.println("22222222 by heshiyuan");
     }
 }
