06ae0d5417fcf1afeb96c15caa78540446a3b85a
city6789 <24662611+city6789@users.noreply.github.com>
2019-10-20 14:49:40 +0800

    Update TestGit2.java

--- src/main/java/cn/itcast/testGIT/TestGit2.java
+++ src/main/java/cn/itcast/testGIT/TestGit2.java
@@ -3,6 +3,7 @@ package cn.itcast.testGIT;
 public class TestGit2 {
 	public static void main(String[] args) {
 		System.out.println("这是一个测试类TEST2");
+		System.out.println("这是一个测试类TEST2第二次修改");
 	}
 
 }
