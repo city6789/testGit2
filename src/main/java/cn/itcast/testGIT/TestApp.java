bc3ebf5f91e9a79eec09f0a59eff797af62d3b74
hesy2019 <heshiyang2019@163.com>
2019-10-20 13:48:24 +0800

    init  project

---
+++ src/main/java/cn/itcast/testGIT/TestApp.java
@@ -0,0 +1,14 @@
+package cn.itcast.testGIT;
+
+/**
+ * 
+ * @author Administrator
+ *
+ */
+public class TestApp {
+	
+	public static void main(String[] args) {
+		System.out.println("this is a test class!");
+	}
+
+}
