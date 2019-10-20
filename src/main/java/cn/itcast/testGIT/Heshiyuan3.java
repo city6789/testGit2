18eb8f6973444d19d1d80df1534a27c916293581
heshiyuan163 <15801636814@163.com>
2019-10-20 16:39:57 +0800

    test  multi add and  update

---
+++ src/main/java/cn/itcast/testGIT/Heshiyuan3.java
@@ -0,0 +1,7 @@
+package cn.itcast.testGIT;
+
+public class Heshiyuan3 {
+    public static void main(String[] args){
+        System.out.println("this is heshiyuan3.java!");
+    }
+}
--- src/main/java/cn/itcast/testGIT/TestGit3.java
+++ src/main/java/cn/itcast/testGIT/TestGit3.java
@@ -6,5 +6,15 @@ public class TestGit3 {
         System.out.println("3333by heshiyang");
         System.out.println("22222222 by heshiyuan");
         System.out.println("444444444444 by heshiyuan");
+        int a=1000;
+        int b=200;
+        System.out.println(min(a,b));
+    }
+
+    private static int min(int a, int b) {
+        if(a<b){
+            return a;
+        }
+        return b;
     }
 }
