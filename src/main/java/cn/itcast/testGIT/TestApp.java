1516d2b00fcd2cc5655318b6e6e9d4b8c39fa907
heshiyuan163 <15801636814@163.com>
2019-10-20 16:57:00 +0800

    update  TestApp.java

--- src/main/java/cn/itcast/testGIT/TestApp.java
+++ src/main/java/cn/itcast/testGIT/TestApp.java
@@ -1,9 +1,7 @@
 package cn.itcast.testGIT;
 
 /**
- * 
  * @author Administrator
- *
  */
 public class TestApp {
 	
@@ -11,6 +9,13 @@ public class TestApp {
 		System.out.println("this is a test class!");
 		System.out.println("1111111111111 by heshiyuan");
 		System.out.println("222222222222 by heshiyuan");
+		int[] arr = new int[]{1,2,3,4,5,6,7,8,9};
+		printArr(arr);
 	}
 
+	public static void  printArr(int[] arr){
+		for(int i=0;i<arr.length;i++){
+			System.out.println("arr["+i+"]="+arr[i]);
+		}
+	}
 }
