78871dcc210f377d047bae8f962963e0b8e02822
hesy2019 <heshiyang2019@163.com>
2019-10-20 16:38:07 +0800

    这是在dev_heshiyang分支的最新的提交,验证分支合并

---
+++ src/main/java/cn/itcast/dao/testGIT/GitDao.java
@@ -0,0 +1,12 @@
+package cn.itcast.dao.testGIT;
+
+public class GitDao {
+
+	/**
+	 * 这是一个默认的工具类，测试类
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("this is a dao,my demo");
+	}
+}
