5f50aa3c8d36668655aa515e1896fd11a58b326b
hesy2019 <heshiyang2019@163.com>
2019-10-20 15:03:58 +0800

    第三次修改

--- src/main/java/cn/itcast/testGIT/TestGit2.java
+++ src/main/java/cn/itcast/testGIT/TestGit2.java
@@ -4,6 +4,7 @@ public class TestGit2 {
 	public static void main(String[] args) {
 		System.out.println("这是一个测试类TEST2");
 		System.out.println("这是一个测试类TEST2第二次修改");
+		System.out.println("这是一个测试类TEST2第三次本地修改");
 	}
 
 }
