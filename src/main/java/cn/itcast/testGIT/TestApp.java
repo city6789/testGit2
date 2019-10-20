d8cf1f1ce825812b44d67c673ac61dcdcec8eed4
hesy2019 <heshiyang2019@163.com>
2019-10-20 16:56:29 +0800

    TestApp.java is update by heshiyang

--- src/main/java/cn/itcast/testGIT/TestApp.java
+++ src/main/java/cn/itcast/testGIT/TestApp.java
@@ -11,6 +11,17 @@ public class TestApp {
 		System.out.println("this is a test class!");
 		System.out.println("1111111111111 by heshiyuan");
 		System.out.println("222222222222 by heshiyuan");
+		
+		int a =1;
+		int b=2;
+		System.out.println("min is"+min(a,b));
 	}
+	
+    private static int min(int a, int b) {
+        if(a<b){
+            return a;
+        }
+        return b;
+    }
 
 }
