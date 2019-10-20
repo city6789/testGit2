bc767799f3bf6db992ff985f5b34548d4233525d
hesy2019 <heshiyang2019@163.com>
2019-10-20 15:06:55 +0800

    第四次修改

--- src/main/java/cn/itcast/testGIT/TestGit2.java
+++ src/main/java/cn/itcast/testGIT/TestGit2.java
@@ -5,6 +5,7 @@ public class TestGit2 {
 		System.out.println("这是一个测试类TEST2");
 		System.out.println("这是一个测试类TEST2第二次修改");
 		System.out.println("这是一个测试类TEST2第三次本地修改");
+		System.out.println("这是测试类TEST2第四次本地修改");
 	}
 
 }
